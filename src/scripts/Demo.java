8923fc6552857627bfbbc9605881e402cc2f5661
sudhirkrishna <sudhirkrishna@SUDHIRKRISHNA>
2019-08-29 14:34:46 +0530

    implementation

---
+++ src/scripts/Demo.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void deemo1()
+	{
+		Reporter.log("hjkjhjkj",true);
+	}
+
+}
